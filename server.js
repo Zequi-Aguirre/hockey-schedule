f2a0b3962bc55b9e1df1bad0e33191e76f830e39
Zequi-Aguirre <zequi4real@gmail.com>
2026-04-17 13:46:38 -0400

    Fix Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/api/league', async (req, res) => {
 // Serve React build in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')));
-  app.get('*', (req, res) => {
+  app.get('*splat', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
